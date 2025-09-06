c88975c818a72d23950e3c3981d230850696447b
Raul Hristov <rauhristov883@gmail.com>
2025-09-06 18:41:09 +0300

    c#_restaurant
    - Moved the files inside other folder to keep project's workspace clean.

---
+++ c#_restaurant/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task01;
+    internal class Program
+    {
+        static void Main()
+        {
+            Restorant restaurant = new Restorant("Raul's Place");
+            Console.WriteLine($"Created a resturant with the name: {restaurant}");
+
+            restaurant.menu.AddDish(new MainCourse("Steak", 12.50m));
+            restaurant.menu.AddDish(new Dessert("Icecream", 5.99m));
+            restaurant.menu.AddDish(new Drink("Coca-Cola", 4.99m));
+
+            restaurant.menu.PrintMenu();
+
+            Waiter Raul = new Waiter("Raul", restaurant);
+            restaurant.AddWaiter(Raul);
+
+            Order customerOrder1 = Raul.TakeOrder();
+
+            Raul.ServeOrder(customerOrder1);
+        }
+    }
---
+++ c#_restaurant/Waiter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Waiter : Restorant
+    {
+        public string Name { get; }
+        public Restorant Restorant;
+
+        public Waiter(string name, Restorant resturant)
+        {
+            Name = name;
+            this.Restorant = resturant;
+        }
+
+        public Order TakeOrder(List<Dish> orderedDishes)
+        {
+            Order order = new Order();
+            foreach (var name in orderedDishes)
+            {
+                var dish = restaurant.Menu.GetDishByName(name);
+
+                if (dish != null) order.AddDish(dish);
+                else Console.WriteLine($"Dish '{name}' not found in menu.");
+            }
+            restaurant.AddOrder(order);
+            return order;
+        }
+
+        public void ServeOrder(Order order) => order.Serving();
+    }
+}
